ca37e8581b64a02ff6c301ad6297919a078823d0
Elvinooooo <Elvinooooo@github.com>
2024-01-16 02:10:38 -0800

    changed the position of print_array

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,7 +23,7 @@ void selection_sort(int *array, size_t size)
 			temp =  array[i];
 			array[i] = array[minimum_index];
 			array[minimum_index] = temp;
+			print_array(array, size);
 		}
-		print_array(array, size);
 	}
 }
